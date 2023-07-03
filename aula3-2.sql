2c22fe7cd0db86f2f192daa3001667e35a64c7c5
Pamela borges <pamela.apolinario@yahoo.com>
2023-07-03 20:55:21 -0300

    Update aula3-2.sql

--- aula3-2.sql
+++ aula3-2.sql
@@ -1,1 +1,31 @@
 
+
+-- Primary Key--
+-- Tabela "usuarios"
+ALTER TABLE usuarios
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "destinos"
+ALTER TABLE destinos
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "reservas"
+ALTER TABLE reservas
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Exemplos --
+
+-- Inserção na tabela "usuarios"
+INSERT INTO usuarios (nome, email, data_nascimento, endereco)
+VALUES ('João Maria', 'joaomaria@example.com', '1990-01-01', 'Rua A, 123');
+
+-- Inserção na tabela "destinos"
+INSERT INTO destinos (nome, descricao)
+VALUES ('Praia Teste', 'Destino paradisíaco com belas praias.');
+
+-- Inserção na tabela "reservas"
+INSERT INTO reservas (id_usuario, id_destino, data, status)
+VALUES (4, 4, '2023-07-01', 'pendente');
