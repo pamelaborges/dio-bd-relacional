c6d1d062700254e2687f2687491de8c84ab54df3
Pamela borges <pamela.apolinario@yahoo.com>
2023-07-03 20:04:24 -0300

    Update aula3-1.sql

--- aula3-1.sql
+++ aula3-1.sql
@@ -17,5 +17,21 @@ CREATE TABLE viagens.reservas (
   id_usuario INT COMMENT 'Referência ao ID do usuário que fez a reserva',
   id_destino INT COMMENT 'Referência ao ID do destino da reserva',
   data DATE COMMENT 'Data da reserva',
-  status VARCHAR(255) COMMENT 'Status da reserva (confirmada, pendente, cancelada, etc.)'
+  status VARCHAR(255) DEFAULT 'pendente' COMMENT 'Status da reserva (confirmada, pendente, cancelada, etc.)'
 );
+
+-- Inserts --
+INSERT INTO usuarios (id, nome, email, data_nascimento, endereco) VALUES 
+(1, 'João Silva', 'joao@example.com', '1990-05-15', 'Rua A, 123'),
+(2, 'Maria Santos', 'maria@example.com', '1985-08-22', 'Avenida B, 456'),
+(3, 'Pedro Souza', 'pedro@example.com', '1998-02-10', 'Rua C, 789');
+
+INSERT INTO viagens.destinos (id, nome, descricao) VALUES 
+(1, 'Praia das Tartarugas', 'Uma bela praia com areias brancas e mar cristalino'),
+(2, 'Cachoeira do Vale Verde', 'Uma cachoeira exuberante cercada por natureza'),
+(3, 'Cidade Histórica de Pedra Alta', 'Uma cidade rica em história e arquitetura');
+
+INSERT INTO viagens.reservas (id, id_usuario, id_destino, data, status) VALUES 
+(1, 1, 2, '2023-07-10', 'confirmada'),
+(2, 2, 1, '2023-08-05', 'pendente'),
+(3, 3, 3, '2023-09-20', 'cancelada');
