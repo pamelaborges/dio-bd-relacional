e527522167b9c0f8b70978cf3d815145be32036b
Pamela borges <pamela.apolinario@yahoo.com>
2023-07-03 20:54:16 -0300

    Update aula3-1.sql

--- aula3-1.sql
+++ aula3-1.sql
@@ -21,7 +21,7 @@ CREATE TABLE viagens.reservas (
 );
 
 -- Inserts --
-INSERT INTO usuarios (id, nome, email, data_nascimento, endereco) VALUES 
+INSERT INTO viagens.usuarios (id, nome, email, data_nascimento, endereco) VALUES 
 (1, 'João Silva', 'joao@example.com', '1990-05-15', 'Rua A, 123'),
 (2, 'Maria Santos', 'maria@example.com', '1985-08-22', 'Avenida B, 456'),
 (3, 'Pedro Souza', 'pedro@example.com', '1998-02-10', 'Rua C, 789');
@@ -35,3 +35,57 @@ INSERT INTO viagens.reservas (id, id_usuario, id_destino, data, status) VALUES
 (1, 1, 2, '2023-07-10', 'confirmada'),
 (2, 2, 1, '2023-08-05', 'pendente'),
 (3, 3, 3, '2023-09-20', 'cancelada');
+
+-- Selects --
+
+-- Selecionar todos os registros da tabela "usuarios"
+SELECT * FROM usuarios;
+
+-- Selecionar apenas o nome e o email dos usuários
+SELECT nome, email FROM usuarios;
+
+-- Selecionar os usuários que possuem o nome "João Silva"
+SELECT * FROM usuarios WHERE nome = 'João Silva';
+
+-- Selecionar os usuários que nasceram antes de uma determinada data
+SELECT * FROM usuarios WHERE data_nascimento < '1990-01-01';
+
+-- Like
+SELECT * FROM usuarios WHERE nome LIKE '%Silva%';
+SELECT * FROM usuarios WHERE nome LIKE 'Jo_o%';
+
+-- Update --
+UPDATE usuarios SET endereco = 'Nova Rua, 123' WHERE email = 'joao@example.com';
+
+-- delete --
+DELETE FROM reservas WHERE status = 'cancelada';
+
+-- Primary Key--
+-- Tabela "usuarios"
+ALTER TABLE usuarios
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "destinos"
+ALTER TABLE destinos
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Tabela "reservas"
+ALTER TABLE reservas
+MODIFY COLUMN id INT AUTO_INCREMENT,
+ADD PRIMARY KEY (id);
+
+-- Exemplos --
+
+-- Inserção na tabela "usuarios"
+INSERT INTO usuarios (nome, email, data_nascimento, endereco)
+VALUES ('João Maria', 'joaomaria@example.com', '1990-01-01', 'Rua A, 123');
+
+-- Inserção na tabela "destinos"
+INSERT INTO destinos (nome, descricao)
+VALUES ('Praia Teste', 'Destino paradisíaco com belas praias.');
+
+-- Inserção na tabela "reservas"
+INSERT INTO reservas (id_usuario, id_destino, data, status)
+VALUES (4, 4, '2023-07-01', 'pendente');
