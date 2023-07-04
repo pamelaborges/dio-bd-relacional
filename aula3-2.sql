9fc3d7a5311e4a10cb673fc073378dae3a382908
Pamela borges <pamela.apolinario@yahoo.com>
2023-07-03 21:08:12 -0300

    Update aula3-2.sql

--- aula3-2.sql
+++ aula3-2.sql
@@ -29,3 +29,24 @@ VALUES ('Praia Teste', 'Destino paradisíaco com belas praias.');
 -- Inserção na tabela "reservas"
 INSERT INTO reservas (id_usuario, id_destino, data, status)
 VALUES (4, 4, '2023-07-01', 'pendente');
+
+-- Chaves estrangeiras --
+
+-- Adicionando chave estrangeira na tabela "reservas" referenciando a tabela "usuarios"
+ALTER TABLE reservas
+ADD CONSTRAINT fk_reservas_usuarios
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id);
+
+-- Adicionando chave estrangeira na tabela "reservas" referenciando a tabela "destinos"
+ALTER TABLE reservas
+ADD CONSTRAINT fk_reservas_destinos
+FOREIGN KEY (id_destino) REFERENCES destinos(id);
+
+-- Alterando a restrição da chave estrangeira "fk_reservas_usuarios" na tabela "reservas" para ON DELETE CASCADE
+ALTER TABLE reservas
+DROP FOREIGN KEY fk_reservas_usuarios;
+
+ALTER TABLE reservas
+ADD CONSTRAINT fk_reservas_usuarios
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+ON DELETE CASCADE;
