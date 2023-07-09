a257415de32e52dc566f4c163d1fa4b5907cd7c2
Pamela borges <pamela.apolinario@yahoo.com>
2023-07-09 15:15:31 -0300

    Update aula3.2.sql

--- aula3.2.sql
+++ aula3.2.sql
@@ -32,9 +32,3 @@ SELECT * FROM usuarios WHERE data_nascimento < '1990-01-01';
 -- Like
 SELECT * FROM usuarios WHERE nome LIKE '%Silva%';
 SELECT * FROM usuarios WHERE nome LIKE 'Jo_o%';
-
--- Update --
-UPDATE usuarios SET endereco = 'Nova Rua, 123' WHERE email = 'joao@example.com';
-
--- delete --
-DELETE FROM reservas WHERE status = 'cancelada';
