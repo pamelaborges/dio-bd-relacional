f529f0eaeafde60157473ab266717bae839235af
Pamela borges <pamela.apolinario@yahoo.com>
2023-07-11 20:47:04 -0300

    Update aula5-1.sql

--- aula5-1.sql
+++ aula5-1.sql
@@ -28,16 +28,16 @@ LEFT JOIN reservas rs
 -- Usuários que não fizeram nenhuma reserva
 SELECT nome
 FROM usuarios
-WHERE id NOT IN (SELECT usuario_id FROM reservas);
+WHERE id NOT IN (SELECT id_usuario FROM reservas);
 
 -- Subconsulta para encontrar os destinos menos populares (com menos reservas):
 
 SELECT nome
 FROM destinos
-WHERE id NOT IN (SELECT destino_id FROM reservas)
+WHERE id NOT IN (SELECT id_destino FROM reservas)
 ORDER BY id;
 
 -- contagem de reservas por usuario
 
-SELECT nome, (SELECT COUNT(*) FROM reservas WHERE usuario_id = usuarios.id) AS total_reservas
+SELECT nome, (SELECT COUNT(*) FROM reservas WHERE id_usuario = usuarios.id) AS total_reservas
 FROM usuarios;
